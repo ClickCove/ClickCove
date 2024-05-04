8d95e8a2e2be28296c4df36e58613a0e3b15038f
Jatin Jaglan <122517751+JatinJaglan347@users.noreply.github.com>
2024-05-04 10:37:25 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
-import Signup from './components/Signup/Signup.jsx'
-import Credential from './components/Credential/Credential.jsx'
+
+import Login from './components/Login/Login.jsx'
+import { createBrowserRouter ,RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home.jsx';
+import Signup from './components/Signup/Signup.jsx';
+
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/SignUP",
+      element:<Signup/>
+    }
+  ])
   return (
     <>
-    
-    {/* <Signup/> */}
-    <Credential/>
+      <RouterProvider router={router}/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+import './index.css';
 
-import Signup from './components/Signup/Signup.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Credential from './components/Credential/Credential.jsx'
-import Login from './components/Login/Login.jsx'
-import Home from './components/Home/Home.jsx'
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Credential/>,
-    children:[
-      {
-        path:"/",
-        element:<Login/>
-      },
-      {
-        path:"/Signup",
-        element:<Signup/>
-      },
-      {
-        path:"/Login",
-        element:<Login/>
-      },
-      {
-        path:"/Home",
-        element:<Home/>
+import Login from './components/Login/Login.jsx';
+import Signup from './components/Signup/Signup.jsx';
+import Home from './components/Home/Home.jsx';
 
-      }
-    ]
-  }
-])
+import {FirebaseProvider} from './context/Firebase'
+import App from './App.jsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Signup/> */}
-    <RouterProvider router={router}/>
-    
-  </React.StrictMode>,
-)
+    <FirebaseProvider>
+      <App/>
+    </FirebaseProvider>
+  </React.StrictMode>
+);
+
